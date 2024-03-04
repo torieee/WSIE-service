24fbdd4db49dcf92ba81dd29bd0f0cdb18f9d0d1
Victoria Thacker <86418527+torieee@users.noreply.github.com>
2024-03-04 11:16:34 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,4 +13,4 @@ mongoose.connect(connectionUri, {
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
     console.log("_____________________________");
-});
+}); 
